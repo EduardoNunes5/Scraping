ccfbf34a94f568092caa6f3c586ca2f4bb72fcfb
Eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-09-04 22:13:30 -0300

    javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"entity","l":"Product"},{"p":"scraper","l":"Scraper"},{"p":"entity","l":"TechnicalInformation"},{"p":"util","l":"Util"}]
